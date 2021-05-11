5574d540ffac5189a7ac07e28f6e60ec17124b30
David Vassallo <dvassallo4427@gmail.com>
2021-05-10 21:53:26 -0400

    adding log analytics

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,20 +9,41 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
+ from opencensus.ext.azure.log_exporter import AzureLogHandler
+ from opencensus.ext.azure import metrics_exporter
+ from opencensus.stats import aggregation as aggregation_module
+ from opencensus.stats import measure as measure_module
+ from opencensus.stats import stats as stats_module
+ from opencensus.stats import view as view_module
+ from opencensus.tags import tag_map as tag_map_module
+ from opencensus.ext.azure.trace_exporter import AzureExporter
+ from opencensus.trace.samplers import ProbabilitySampler
+ from opencensus.trace.tracer import Tracer
+ from opencensus.ext.flask.flask_middleware import FlaskMiddleware
 
 # Logging
-logger = # TODO: Setup logger
+logger = logging.getLogger(__name__)
+logger.addHandler(AzureLogHandler(connection_string='InstrumentationKey=3f5b4bfb-d2f3-4bad-bfe8-f4bd35815e59'))
 
 # Metrics
-exporter = # TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+  enable_standard_metrics=True,
+  connection_string='InstrumentationKey=3f5b4bfb-d2f3-4bad-bfe8-f4bd35815e59')
 
 # Tracing
-tracer = # TODO: Setup tracer
-
+tracer = Tracer(
+    exporter=AzureExporter(
+        connection_string='InstrumentationKey=3f5b4bfb-d2f3-4bad-bfe8-f4bd35815e59'),
+    sampler=ProbabilitySampler(1.0),
+)
 app = Flask(__name__)
 
 # Requests
-middleware = # TODO: Setup flask middleware
+middleware = FlaskMiddleware(
+    app,
+    exporter=AzureExporter(connection_string="InstrumentationKey=3f5b4bfb-d2f3-4bad-bfe8-f4bd35815e59"),
+    sampler=ProbabilitySampler(rate=1.0),
+)
 
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
@@ -60,8 +81,10 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
+        tracer.span(name="Cats")
         # TODO: use tracer object to trace cat vote
         vote2 = r.get(button2).decode('utf-8')
+        tracer.span(name="Dogs")
         # TODO: use tracer object to trace dog vote
 
         # Return index with values
